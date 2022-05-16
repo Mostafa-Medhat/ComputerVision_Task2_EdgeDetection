4a908559f6dc6611a4cdf26d256e115524345d29
Abdelrahman Khaled <61363091+AbdelrahmanKK@users.noreply.github.com>
2022-05-17 00:29:54 +0200

    Mn3n3's commit

--- main.py
+++ main.py
@@ -6,36 +6,35 @@ import sys
 import cv2 as cv
 
 
-def main():
-    window_name = ('Sobel Demo - Simple Edge Detector')
-    scale = 1
-    delta = 0
-    ddepth = cv.CV_16S
+
+window_name = ('Sobel Demo - Simple Edge Detector')
+scale = 1
+delta = 0
+ddepth = cv.CV_16S
 
 
-    # Load the image
-    src = cv.imread("lena.png", cv.IMREAD_COLOR)
-    # Check if image is loaded fine
+# Load the image
+src = cv.imread("gradients.jpg", cv.IMREAD_COLOR)
+# Check if image is loaded fine
 
 
-    src = cv.GaussianBlur(src, (3, 3), 0)
+src = cv.GaussianBlur(src, (3, 3), 0)
 
-    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
 
-    grad_x = cv.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
-    # Gradient-Y
-    # grad_y = cv.Scharr(gray,ddepth,0,1)
-    grad_y = cv.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+grad_x = cv.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
+# Gradient-Y
+# grad_y = cv.Scharr(gray,ddepth,0,1)
+grad_y = cv.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
 
-    abs_grad_x = cv.convertScaleAbs(grad_x)
-    abs_grad_y = cv.convertScaleAbs(grad_y)
+abs_grad_x = cv.convertScaleAbs(grad_x)
+abs_grad_y = cv.convertScaleAbs(grad_y)
 
-    grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+grad = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
 
-    cv.imshow(window_name, grad)
-    cv.waitKey(0)
-
-    return 0
+cv.imshow(window_name, grad)
+cv.waitKey(0)
 
 
-main()
+
+
